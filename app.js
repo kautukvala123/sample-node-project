a85a909291226adb594576dec10393e09c0087f6
kautuk vala <kautukvala007@gmail.com>
2025-04-01 17:41:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,5 +21,5 @@ HTTP SERVER OLUŞTURULDU
 
 */
 http.createServer(app).listen(app.get('port'), function(){
-	console.log('Sistem ' + app.get('port') + ' tmy name is kautuk.');
+	console.log('Sistem ' + app.get('port') + ' tmy name is kautuk. chut ka bhut');
 });
